67de3d50aaa5ae1181976aa5ae4bef5c6f9ccb24
Xander Lopez <xanderlopez@me.com>
2020-05-09 16:03:36 +1000

    Restaurant class

---
+++ app/src/main/java/com/xanderlopez/foodster/Restaurant.java
@@ -0,0 +1,51 @@
+package com.xanderlopez.foodster;
+
+public class Restaurant {
+    /* Declare private variables */
+
+    private int id;
+    private String name, description, image;
+
+    /* Constructor */
+
+    public Restaurant(int id, String name, String description, String image) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+    /* Getter and setter methods */
+
+    public int getId() {
+        return id;
+    }
+
+    public  void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
