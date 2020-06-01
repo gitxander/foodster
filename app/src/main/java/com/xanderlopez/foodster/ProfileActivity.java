f1c52473c832a22984897f3971498f30ef175efb
Xander Lopez <xanderlopez@me.com>
2020-06-01 19:51:20 +1000

    Fix sign up -> sign in

--- app/src/main/java/com/xanderlopez/foodster/ProfileActivity.java
+++ app/src/main/java/com/xanderlopez/foodster/ProfileActivity.java
@@ -171,7 +171,7 @@ public class ProfileActivity extends AppCompatActivity {
                         // Sign in success, update UI with the signed-in user's information
                         Log.d(TAG, "createUserWithEmail:success");
                         FirebaseUser user = mAuth.getCurrentUser();
-                        setContentView(R.layout.profile_signin);
+                        signin();
                     } else {
                         // If sign in fails, display a message to the user.
                         Log.w(TAG, "createUserWithEmail:failure", task.getException());
